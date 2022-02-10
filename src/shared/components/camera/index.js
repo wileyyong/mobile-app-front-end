9061cb3847e8b927daf895e4b5b85f1f30fcb01b
JL <johnylawrence1987@gmail.com>
2022-02-10 04:00:20 -0800

    remove expo camera

--- src/shared/components/camera/index.js
+++ src/shared/components/camera/index.js
@@ -86,27 +86,12 @@ const PozzleCamera = () => {
   return (
     <>
       <View style={styles.cameraContainer}>
-        {
+        
           <RNCamera
             ref={cameraRef}
             style={styles.image}
-            type={RNCamera.Constants.Type.back}
-            flashMode={RNCamera.Constants.FlashMode.on}
-            androidCameraPermissionOptions={{
-              title: 'Permission to use camera',
-              message: 'We need your permission to use your camera',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}
-            androidRecordAudioPermissionOptions={{
-              title: 'Permission to use audio recording',
-              message: 'We need your permission to use your audio',
-              buttonPositive: 'Ok',
-              buttonNegative: 'Cancel',
-            }}
           />
-        }
-
+        
         <View style={styles.cameraButtonContainer}>
           <TouchableOpacity style={positionButtonStyle} onPress={changeCameraDevice}>
             <CameraIcon color={cameraPositionIconColor} />
