5493382c0d488f8df36a3b4a196153954a387bc4
maymax777 <dev.bugger777@yahoo.com>
2022-02-23 07:38:41 -0500

    Add user interaction

--- src/screens/planet/index.js
+++ src/screens/planet/index.js
@@ -1,19 +1,22 @@
 import { Planet } from '$components';
 
-import React from 'react';
-import { View } from 'react-native';
+import React, { useRef, useState} from 'react';
 import { Canvas } from '@react-three/fiber/native';
+import OrbitControlsView from 'expo-three-orbit-controls';
 
 const PlanetScreen = () => {
+  const orbitControlRef = useRef(null);
+  const [camera, setCamera] = useState(null);
+
   return (
-    <View style={{ flex: 1 }}>
-      <Canvas>
+    <OrbitControlsView camera={camera} ref={orbitControlRef} style={{ flex: 1 }}>
+      <Canvas onCreated={({ camera }) => setCamera(camera)}>
         <color args={['#000']} attach="background" />
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
         <Planet position={[0, 0, 0]} size={1} speed={1} />
       </Canvas>
-    </View>
+    </OrbitControlsView>
   );
 };
 
