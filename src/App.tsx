2b3d87a718b92b6eb46a0ddd949cfe883227c6f4
JohnyLawrence <johnylawrence1987@gmail.com>
2022-03-11 11:03:24 +0000

    fix lint issues

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React from 'react';
 import { StatusBar, Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 
 import NavigationRoot from './navigation';
-import { Provider } from 'react-redux';
 import store from './redux/index';
 
 export default function App() {
