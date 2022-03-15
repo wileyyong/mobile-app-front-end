abb52788e4f7d123b5b9faa958f22c58a2bb3c18
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2022-03-15 14:58:21 +0100

    Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,110 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:jest/recommended', 'prettier', 'prettier/react'],
+  globals: {
+    __DEV__: false,
+    fetch: false,
+    window: true,
+  },
+  parser: 'babel-eslint',
+  rules: {
+    'linebreak-style': 0,
+    'no-shadow': 'off',
+    'import/export': 'off',
+    'import/named': 'off',
+    'import/no-unresolved': [
+      'error',
+      {
+        ignore: [
+          '^\\$assets',
+          '^\\$auth',
+          '^\\$business-layer',
+          '^\\$components',
+          '^\\$constants',
+          '^\\$theme',
+          '^\\$screens',
+          '^\\$services',
+          '^\\$utils',
+          '^\\$web3',
+          '^\\$widgets',
+        ],
+      },
+    ],
+    'import/no-extraneous-dependencies': 0,
+    'import/order': [0, { 'newlines-between': 'always' }],
+    'import/prefer-default-export': 'off',
+    'no-prototype-builtins': 'off',
+    'no-underscore-dangle': 'warn',
+    'no-use-before-define': ['error', 'nofunc'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['const', 'let'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let'],
+        next: ['const', 'let'],
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'class',
+      },
+      {
+        blankLine: 'always',
+        prev: 'class',
+        next: '*',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'multiline-block-like',
+      },
+      {
+        blankLine: 'always',
+        prev: 'multiline-expression',
+        next: '*',
+      },
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+    'prefer-destructuring': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'static-methods',
+          'instance-variables',
+          'lifecycle',
+          'everything-else',
+          'render',
+        ],
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.ts', '.tsx', '.jsx'] },
+    ],
+    'react/jsx-sort-props': [0, { callbacksLast: true }],
+    'sort-keys': [0, 'asc', { caseSensitive: true }],
+    'sort-vars': 0,
+  },
+  settings: {
+    'import/core-modules': [
+      '$auth',
+      '$assets',
+      '$business-layer',
+      '$components',
+      '$constants',
+      '$screens',
+      '$services',
+      '$theme',
+      '$utils',
+      '$web3',
+      '$widgets',
+    ],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
