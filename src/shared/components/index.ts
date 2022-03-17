a4aa9e77dfb4d6610322f0e81a726a4a06ccf361
JohnyLawrence <johnylawrence1987@gmail.com>
2022-03-17 09:38:48 +0000

    fix uppercases imports

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,23 +1,23 @@
-import Button, { IconButton } from './Buttons';
+import Button, { IconButton } from './buttons';
 export { default as BackButton } from './back-button';
 export { default as ProgressButton } from './progress-button';
 export { default as Text } from './text';
 export { default as CosmicBackground } from './cosmic-background';
-export { default as Orbs } from './Orbs';
+export { default as Orbs } from './orbs';
 export { default as Modal } from './modal';
-export { default as Input } from './Input';
+export { default as Input } from './input';
 export { default as BlurView } from './blur-view';
-export { default as Toast } from './Toast';
+export { default as Toast } from './toast';
 export { default as Camera } from './camera';
 export { default as Planet } from './planet';
 export { default as Ticket } from './ticket';
-export { default as Earth } from './Earth';
+export { default as Earth } from './earth';
 export { default as ImageBackground } from './image-background';
 export { default as Image } from './image';
 export { default as OrbitControlsView } from './orbit-control';
 
 export { default as PozzleCameraButtons } from './camera/camera-buttons/index';
 
-export * from './Icons';
-export * from './Stacks';
+export * from './icons';
+export * from './stacks';
 export { Button, IconButton };
