891c95e5bfb711d01afbd42c4165dae5e9e37a47
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2022-03-17 05:01:47 +0100

    Delete Stacks

--- src/shared/components/Stacks/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as HStack } from './subcomponents/h-stack';
-export { default as VStack } from './subcomponents/v-stack';
-export { default as Spacer } from './subcomponents/spacer';
