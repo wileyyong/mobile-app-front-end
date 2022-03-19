d00a1b6361157c1d801e4b8fc558ef5356357fa5
JohnyLawrence <johnylawrence1987@gmail.com>
2022-03-19 11:55:57 +0000

    fix modal usage

--- src/screens/video/index.tsx
+++ src/screens/video/index.tsx
@@ -63,45 +63,44 @@ const VideoScreen = () => {
       onClose={() => {
         navigation.goBack();
       }}
-      show={true}
-      children={
-        <View style={[styles.container, { width }]}>
-          <ImageBackground source={radialGradient} style={styles.image}>
-            <VideoFeed
-              videos={videos}
-              loadMore={getVideos}
-              onPressBack={navigation.goBack}
-            />
+      show={true}>
+      <View style={[styles.container, { width }]}>
+        <ImageBackground source={radialGradient} style={styles.image}>
+          <VideoFeed
+            videos={videos}
+            loadMore={getVideos}
+            onPressBack={navigation.goBack}
+          />
 
-            <View style={styles.buttonContainer}>
-              <Button
-                backgroundColor={Colors.WHITE}
-                disabled={false}
-                size="small-plus">
-                <View style={{ alignItems: 'center', flexDirection: 'row' }}>
-                  <Image source={pledgeIcon} style={styles.buttonImage} />
-                  <Text size="xs" weight="bold">
-                    {t('videoScreen.pledgeText')}
-                  </Text>
-                </View>
-              </Button>
+          <View style={styles.buttonContainer}>
+            <Button
+              backgroundColor={Colors.WHITE}
+              disabled={false}
+              size="small-plus">
+              <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+                <Image source={pledgeIcon} style={styles.buttonImage} />
+                <Text size="xs" weight="bold">
+                  {t('videoScreen.pledgeText')}
+                </Text>
+              </View>
+            </Button>
 
-              <Button
-                backgroundColor={Colors.WHITE}
-                disabled={false}
-                size="medium"
-                onPress={launchAddPozzleScreen}>
-                <View style={{ alignItems: 'center', flexDirection: 'row' }}>
-                  <Image source={addPozzleIcon} style={styles.buttonImage} />
-                  <Text size="xs" weight="bold">
-                    {t('videoScreen.addPozzleText')}
-                  </Text>
-                </View>
-              </Button>
-            </View>
-          </ImageBackground>
-        </View>
-      }></Modal>
+            <Button
+              backgroundColor={Colors.WHITE}
+              disabled={false}
+              size="medium"
+              onPress={launchAddPozzleScreen}>
+              <View style={{ alignItems: 'center', flexDirection: 'row' }}>
+                <Image source={addPozzleIcon} style={styles.buttonImage} />
+                <Text size="xs" weight="bold">
+                  {t('videoScreen.addPozzleText')}
+                </Text>
+              </View>
+            </Button>
+          </View>
+        </ImageBackground>
+      </View>
+    </Modal>
   );
 };
 
