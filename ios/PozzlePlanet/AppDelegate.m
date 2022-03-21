44d6481112c61bfc6f97a039d6f01eee028ce733
JohnyLawrence <johnylawrence1987@gmail.com>
2022-03-21 14:32:24 +0000

    Add firebase package

--- ios/PozzlePlanet/AppDelegate.m
+++ ios/PozzlePlanet/AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 
+#import <Firebase.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -41,6 +42,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
