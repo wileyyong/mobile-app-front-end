e4a1c3278085044fe8fbdf5d450a63e6adb2f4f0
JohnyLawrence <johnylawrence1987@gmail.com>
2022-04-13 12:51:55 +0100

    fix presable

--- src/shared/components/progress-button/subcomponents/progress-button-pressable.js
+++ src/shared/components/progress-button/subcomponents/progress-button-pressable.js
@@ -40,12 +40,23 @@ const ProgressButtonPressable = ({ scaleTo, ...props }) => {
       style={[props.style, { transform: [{ scale }] }]}
       onLongPress={() => {
         onLongPress(scaleTo);
+        if (props.pressType === 'LONG') {
+          if (props.onLongPressStart) {
+            props.onLongPressStart();
+          }
+        }
       }}
       onPressIn={() => {
         onPressIn(scaleTo);
       }}
       onPressOut={() => {
         onPressOut(1);
+
+        if (props.pressType === 'LONG') {
+          if (props.onLongPressStop) {
+            props.onLongPressStop();
+          }
+        }
       }}
     />
   );
