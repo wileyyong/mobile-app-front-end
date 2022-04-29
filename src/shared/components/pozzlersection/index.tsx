992aa5a4cf7e3b29d2e43e2a0784fe9087225e7e
samuelokediji <samuelolaideokediji@gmail.com>
2022-04-29 13:09:36 +0100

    remove unneeded imports

--- src/shared/components/pozzlersection/index.tsx
+++ src/shared/components/pozzlersection/index.tsx
@@ -1,5 +1,5 @@
 import { Padding } from '$theme';
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import {
   StyleSheet,
   Text,
@@ -11,35 +11,22 @@ import {
 import Svg, { Text as Tx } from 'react-native-svg';
 import { Hexagon } from '$components';
 import Hex from '../../../assets/icons/hex.svg';
-import Icon from '../../../assets/icons/planeticon.svg';
 import styles from './styles';
-// import { CapitalizeEachWord } from './utils';
-import axios from "axios"
+
+import axios from 'axios';
 interface SectionProps {
   item: any;
 }
 const PozzlersSection = ({ item }: SectionProps) => {
   let len = item.pozzles.length;
 
-  const getUserName = async()=>{
-
-try {
-  let res = await axios.get(``)
-  console.log(res)
-} catch (error) {
-  console.log(error)
-}
-
-  }
-useEffect(() => {
- getUserName()
-}, [])
-
-
   return (
     <View style={styles.section}>
       <View style={styles.header}>
-        <Image style={styles.avatar} source={require("../../../assets/images/pozzlePilot.png")} />
+        <Image
+          style={styles.avatar}
+          source={require('../../../assets/images/pozzlePilot.png')}
+        />
         <View>
           <Text style={styles.title}>{item.pozzler}</Text>
           <View style={styles.holderView}>
@@ -52,14 +39,13 @@ useEffect(() => {
       </View>
       <ScrollView horizontal style={styles.scroll}>
         <View>
-        <FlatList
+          <FlatList
             horizontal
             style={[styles.inner]}
             data={item.pozzles}
             renderItem={({ item }) => <Hexagon pic={item.muxThumbnail} />}
             keyExtractor={item => item._id}
           />
-       
         </View>
       </ScrollView>
     </View>
