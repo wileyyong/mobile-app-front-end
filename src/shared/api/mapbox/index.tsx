3f56761236322608e5c1aec02c1b67a2ec56d1e5
James Wee <jamesweedev@gmail.com>
2022-05-12 12:51:37 -0330

    fix/location-api, redirecting screen as Discover

--- src/shared/api/mapbox/index.tsx
+++ src/shared/api/mapbox/index.tsx
@@ -6,7 +6,7 @@ const MapBox = {
   async translateGPStoLocation(lat: number, lng: number): Promise<string> {
     const _result = await axios
       .get(
-        `${MAPBOX_API_URL}/geocoding/${MAPBOX_API_VERSION}/mapbox.places/${lat},${lng}.json?types=country&access_token=${MAPBOX_ACCESS_TOKEN}`,
+        `${MAPBOX_API_URL}/geocoding/${MAPBOX_API_VERSION}/mapbox.places/${lng},${lat}.json?types=country&access_token=${MAPBOX_ACCESS_TOKEN}`,
       )
     const result = _result.data?.features[0]?.place_name;
     return result;
