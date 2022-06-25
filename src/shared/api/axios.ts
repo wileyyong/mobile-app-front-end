ba6d33c2983be566e9fe66e00ed6b2aef80604f3
JohnyLawrence <johnylawrence1987@gmail.com>
2022-06-25 06:02:12 +0100

    remove console logs

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -10,7 +10,6 @@ const instance = axios.create({
 
 instance.interceptors.request.use(async config => {
   const token = await fetchItemFromStorage('sessionToken');
-  console.log(token);
   if (token) {
     config.headers.authorization = token;
   }
