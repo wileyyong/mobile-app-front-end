4670757f8c6775e0793d0afd0028b55b60adddf7
JohnyLawrence <johnylawrence1987@gmail.com>
2022-07-22 12:59:08 +0100

    small improvements

--- src/shared/components/video/video-view/cache-videos.tsx
+++ src/shared/components/video/video-view/cache-videos.tsx
@@ -1,9 +1,9 @@
 import { IVideoItem } from './utils';
-import convertToProxyURL from 'react-native-video-cache';
+import convert from 'react-native-video-cache';
 
 export const cacheVideo = async (src: string) => {
   try {
-    const r = await convertToProxyURL(src);
+    const r = await convert(src);
     return r;
   } catch {
     return src;
