f1212162874a685f74b9096153d60dd4fcc2c0b3
JohnyLawrence <johnylawrence1987@gmail.com>
2022-07-25 07:48:41 +0100

    Rollback video property

--- src/screens/video/index.tsx
+++ src/screens/video/index.tsx
@@ -124,12 +124,12 @@ const VideoScreen = ({ route }) => {
   ) => {
     await _videosData.forEach(async video => {
       if (isFromNavigation) {
-        if (!video.cachedSrc && video.processedVideoSrc)
-          video.cachedSrc = await cacheVideo(video.processedVideoSrc);
+        if (!video.cachedSrc && video.videoSrc)
+          video.cachedSrc = await cacheVideo(video.videoSrc);
        
       } else {
         if (!video.cachedSrc)
-          video.cachedSrc = await cacheVideo(video.pozzles[0].processedVideoSrc);
+          video.cachedSrc = await cacheVideo(video.pozzles[0].videoSrc);
       }
     });
     return _videosData;
